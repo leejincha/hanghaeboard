f122ddade040262a4f2ffd8ae582d68b041694b0
leejincha <leejincha@gmail.com>
2022-12-04 20:16:29 +0900

    DelResponseDto 추가

--- Hanghaeboard/src/main/java/com/sparta/hanghaeboard/controller/PostController.java
+++ Hanghaeboard/src/main/java/com/sparta/hanghaeboard/controller/PostController.java
@@ -1,12 +1,13 @@
 package com.sparta.hanghaeboard.controller;
 
+import com.sparta.hanghaeboard.dto.DelResponseDto;
 import com.sparta.hanghaeboard.dto.PostRequestDto;
 import com.sparta.hanghaeboard.dto.PostResponseDto;
 import com.sparta.hanghaeboard.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,8 +35,8 @@ public class PostController {
         return postService.update(id, requestDto);
     }
 
-    @DeleteMapping("/api/posts/{id}")
-    public Map<String,Object> deletePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto) {
-        return postService.deletePost(id, requestDto);
+    @DeleteMapping("/api/posts/{id}") // 반환타입을 새로 만든 DelResponseDto로 바꿔주어 봤다.
+    public DelResponseDto deleteMemo(@PathVariable Long id, @RequestBody PostRequestDto requestDto) {
+        return postService.deletePost(id, requestDto.getPassword());//그리고 매개값도 id와 비밀번호만 넣어주었다.
     }
 }
--- Hanghaeboard/src/main/java/com/sparta/hanghaeboard/service/PostService.java
+++ Hanghaeboard/src/main/java/com/sparta/hanghaeboard/service/PostService.java
@@ -1,5 +1,6 @@
 package com.sparta.hanghaeboard.service;
 
+import com.sparta.hanghaeboard.dto.DelResponseDto;
 import com.sparta.hanghaeboard.dto.PostRequestDto;
 import com.sparta.hanghaeboard.dto.PostResponseDto;
 import com.sparta.hanghaeboard.entity.Post;
@@ -9,9 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -62,18 +61,19 @@ public class PostService {
     }
 
     @Transactional
-    public Map<String, Object> deletePost(Long id, PostRequestDto requestDto) {
-        Post post = postRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+    public DelResponseDto deletePost(Long id, String password) { //매개값으로 id와 password만 사용 하였고 반환타입을 Dto로 바꿔주었다.
+        Post post = postRepository.findById(id).orElseThrow( //이 부분은 업데이트와 동일하다.
+                () -> new IllegalArgumentException("해당 게시물이 존재하지 않습니다.")
         );
-        Map<String, Object> reponse = new HashMap<>();
-        if (requestDto.getPassword().equals(post.getPassword())) {
-            postRepository.deleteById(id);
-            reponse.put("success",true);
-            return reponse;
+
+        DelResponseDto result = new DelResponseDto(); //DelResponseDto 를 생성해주고,
+        if (post.getPassword().equals(password)) {     //비밀번호 일치여부를 조건문으로 확인한 뒤
+            postRepository.deleteById(id);                  // 비밀번호가 일치하면 해당 아이디 게시글을 삭제해준다.
+            result.setResult("success", "게시물이 삭제되었습니다."); //성공하면 다음과 같은 반환값을 준다.
+            return result;
         } else {
-            reponse.put("success",false);
-            return reponse;
+            result.setResult("failed", "비밀번호가 일치하지 않습니다"); //비밀번호가 일치하지 않으면 다음과 같은 반환값을 준다.
+            return result;
         }
     }
 }
